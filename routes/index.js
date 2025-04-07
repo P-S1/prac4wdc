81e264805cc16d238f4c7fd085ac69cce30dfe02
P-S1 <pranav.singh232@gmail.com>
2025-04-07 16:31:37 +0930

    test

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,36 @@ router.get('/last.txt', function(req, res) {
 });
 
 
+
+
+let visitCount = 0;
+
+// Array of colors
+const colors = ['red', 'yellow', 'green', 'blue'];
+
+/* GET /color.html */
+router.get('/color.html', function(req, res) {
+  
+  const currentColor = colors[visitCount % colors.length];
+
+  visitCount++;
+  
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Color Page</title>
+    </head>
+    <body>
+      <h1 style="color:${currentColor};">${currentColor}</h1>
+    </body>
+    </html>
+  `);
+});
+
+
+
+
 module.exports = router;
