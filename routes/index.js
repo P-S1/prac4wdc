2d1971c95fad59902c95ea8d8ee6e55e434413b1
P-S1 <pranav.singh232@gmail.com>
2025-04-07 19:08:54 +0930

    test

--- routes/index.js
+++ routes/index.js
@@ -101,4 +101,36 @@ router.get('/log-ro.json', function(req, res) {
 });
 
 
+/* GET /log.html */
+router.get('/log.html', function(req, res) {
+  
+  const currentTimestamp = new Date().toString();
+
+  
+  visitTimestamps.push(currentTimestamp);
+
+ 
+  let timestampList = visitTimestamps.map(timestamp => `<li>${timestamp}</li>`).join('');
+
+  
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Visitor Log</title>
+    </head>
+    <body>
+      <h1>Visitor Log</h1>
+      <ul>
+        ${timestampList}
+      </ul>
+    </body>
+    </html>
+  `);
+});
+
+
+
 module.exports = router;
