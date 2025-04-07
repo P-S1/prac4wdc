632ccc6bbabda2063bdcf9c03f18c2a9f981668b
P-S1 <pranav.singh232@gmail.com>
2025-04-07 17:43:16 +0930

    test

--- routes/index.js
+++ routes/index.js
@@ -57,4 +57,40 @@ router.get('/color.html', function(req, res) {
 
 
 
+let visitTimestamps = [];
+
+/* GET /log.html */
+router.get('/log.html', function(req, res) {
+  
+  const currentTimestamp = new Date().toString();
+
+  
+  visitTimestamps.push(currentTimestamp);
+
+ 
+  let timestampList = visitTimestamps.map(timestamp => `<li>${timestamp}</li>`).join('');
+
+  
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Visitor Log</title>
+    </head>
+    <body>
+      <h1>Visitor Log</h1>
+      <ul>
+        ${timestampList}
+      </ul>
+    </body>
+    </html>
+  `);
+});
+
+
+
+
+
 module.exports = router;
