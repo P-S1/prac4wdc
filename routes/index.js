32d4b8b1ab951159f1665eabce4e7514005832cc
P-S1 <pranav.singh232@gmail.com>
2025-04-07 15:44:02 +0930

    test

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+let lastVisitTimestamp = null;
+
+
+
+router.get('/last.txt', function(req, res) {
+  if (lastVisitTimestamp) {
+    
+    res.send(`Last visited on: ${lastVisitTimestamp}`);
+  } else {
+    
+    res.send('');
+  }
+
+  
+  lastVisitTimestamp = new Date().toISOString();
+});
+
+
 module.exports = router;
